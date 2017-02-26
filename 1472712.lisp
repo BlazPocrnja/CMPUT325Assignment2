1cc5b7613175efcf747b055241998625d2b81c97
BlazPocrnja <bpocrnja@gmail.com>
2017-02-25 18:28:06 -0700

    Added atom

--- 1472712.lisp
+++ 1472712.lisp
@@ -7,6 +7,7 @@
                     ; handle built-in functions
                     ((eq f 'if)  (if (fl-interp (car arg) P) (fl-interp (cadr arg) P) (fl-interp (caddr arg) P)))
                     ((eq f 'null) (null (fl-interp (car arg) P)))
+                    ((eq f 'atom) (atom (fl-interp (car arg) P)))
                     ((eq f 'first)  (car (fl-interp (car arg) P)))
 	       
         	        ; if f is a user-defined function,
